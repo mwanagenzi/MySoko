9cd7135173edbceb5831c3b3af65d090c7929e00
mwanagenzi <njorogemosesngure@gmail.com>
2024-01-22 10:44:51 +0300

    SplashScreen construction complete

--- app/src/main/java/com/mwanagenzi/mysoko/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/mwanagenzi/mysoko/ui/screens/SplashScreen.kt
@@ -1,4 +1,65 @@
 package com.mwanagenzi.mysoko.ui.screens
 
-class SplashScreen {
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mwanagenzi.mysoko.R
+
+@Composable
+fun SplashScreen(modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AppLogo(appLogoDrawable = R.drawable.basket)
+        Spacer(modifier = Modifier.height(18.dp))
+        AppName(name = "My Soko")
+    }
+}
+
+@Composable
+fun AppName(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name",
+        modifier = modifier,
+        style = TextStyle(fontWeight = FontWeight.Medium, fontFamily = FontFamily.SansSerif)
+    )
+}
+
+@Composable
+fun AppLogo(
+    @DrawableRes appLogoDrawable: Int
+) {
+    Image(
+        painter = painterResource(id = appLogoDrawable),
+        contentDescription = "App Logo",
+        modifier = Modifier.size(100.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen(
+        modifier = Modifier
+    )
 }
