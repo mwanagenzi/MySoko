ad2b769c9a911e34f43067e81be1f303ce207962
mwanagenzi <njorogemosesngure@gmail.com>
2024-02-05 16:13:01 +0300

    refact: Include primary UI elements in Product Screen

--- app/src/main/java/com/mwanagenzi/mysoko/ui/screens/ProductScreen.kt
+++ app/src/main/java/com/mwanagenzi/mysoko/ui/screens/ProductScreen.kt
@@ -1,21 +1,72 @@
 package com.mwanagenzi.mysoko.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.mwanagenzi.mysoko.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun ProductScreen() {
+fun ProductScreen(modifier: Modifier) {
     Scaffold(
         topBar = {
             TopAppBar(
                 title = {},
-
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Outlined.Favorite,
+                            contentDescription = "Favourite icon",
+                            tint = Color.Red //todo: set this change if selected
+                        )
+                    }
+                }
             )
         }
     ) {
+        Column(
+            modifier
+                .fillMaxSize()
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.basket),
+                contentDescription = "Product image",
+                modifier.weight(1f)
+            )
+            Surface(
+                modifier
+                    .padding(horizontal = 16.dp)
+                    .weight(1f),
+                shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+            ) {
+                Text(text = "Xbox Series X", style = MaterialTheme.typography.titleMedium)
+                Row {
 
+                }
+            }
+        }
     }
 }
