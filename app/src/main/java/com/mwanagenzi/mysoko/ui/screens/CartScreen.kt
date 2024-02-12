bac9680fa44dcde52b53f912d08343c1efa939e9
mwanagenzi <njorogemosesngure@gmail.com>
2024-02-12 16:02:27 +0300

    refact: Include CartScreen cart item UI construction

--- app/src/main/java/com/mwanagenzi/mysoko/ui/screens/CartScreen.kt
+++ app/src/main/java/com/mwanagenzi/mysoko/ui/screens/CartScreen.kt
@@ -1,4 +1,175 @@
 package com.mwanagenzi.mysoko.ui.screens
 
-class CartScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mwanagenzi.mysoko.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CartScreen(modifier: Modifier) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(
+                    text = "Discover",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }, actions = {
+                IconButton(onClick = {
+                    // todo: Navigate to cart
+                }, enabled = true) {
+                    Icon(
+                        imageVector = Icons.Filled.MoreHoriz,
+                        contentDescription = "Shopping Cart Icon"
+                    )
+                }
+            })
+        }
+    ) {
+        Column(modifier.padding(it)) {
+
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ProductCard(modifier: Modifier) {
+    OutlinedCard {
+        Row(
+            modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.basket),
+                contentDescription = "Cart product image",
+                modifier
+                    .weight(1f)
+                    .size(85.dp)
+                    .background(Color.LightGray, shape = RoundedCornerShape(20.dp))
+                    .padding(vertical = 8.dp),
+            )
+            Column(
+                modifier
+                    .fillMaxWidth()
+                    .padding(start = 8.dp)
+                    .weight(2f),
+                verticalArrangement = Arrangement.SpaceBetween,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row(
+                    modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Xbox Series X")
+                    IconButton(onClick = {
+                        //todo: delete product from cart
+                    }) {
+                        Icon(
+                            imageVector = Icons.Outlined.Delete,
+                            contentDescription = "Remove product from cart"
+                        )
+                    }
+                }
+                Text(
+                    text = "1 TB",
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Light)
+                )
+                Row(
+                    modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "$570.00",
+                        style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.ExtraBold),
+
+                        )
+                    Row(
+                        modifier.padding(end = 8.dp, bottom = 8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(6.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(
+                            onClick = {
+                                //todo: reduce qunatity from cart
+                            },
+                            modifier
+                                .border(
+                                    1.dp,
+                                    color = MaterialTheme.colorScheme.primary,
+                                    shape = RoundedCornerShape(size = 5.dp)
+                                )
+                                .size(30.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Remove,
+                                contentDescription = "Reduce quantity"
+                            )
+                        }
+                        Text(
+                            text = "1",
+                            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Medium)
+                        )
+                        IconButton(
+                            onClick = {
+                                //todo: add quantity product from cart
+                            },
+                            modifier
+                                .border(
+                                    1.dp,
+                                    color = MaterialTheme.colorScheme.primary,
+                                    shape = RoundedCornerShape(size = 5.dp)
+                                )
+                                .size(30.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Add,
+                                contentDescription = "Reduce quantity"
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProductCardPreview() {
+    ProductCard(modifier = Modifier)
 }
