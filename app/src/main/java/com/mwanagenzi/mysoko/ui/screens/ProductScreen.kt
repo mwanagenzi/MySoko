334cebc129957975202cf5b8c3cfff6cce59a73f
mwanagenzi <njorogemosesngure@gmail.com>
2024-02-12 13:24:17 +0300

    refact: Include product name, rating and description UI elements on Product Screen

--- app/src/main/java/com/mwanagenzi/mysoko/ui/screens/ProductScreen.kt
+++ app/src/main/java/com/mwanagenzi/mysoko/ui/screens/ProductScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.StarRate
 import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.AssistChip
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.mwanagenzi.mysoko.R
 
@@ -62,11 +65,40 @@ fun ProductScreen(modifier: Modifier) {
                     .weight(1f),
                 shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
             ) {
-                Text(text = "Xbox Series X", style = MaterialTheme.typography.titleMedium)
-                Row {
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Xbox Series X", style = MaterialTheme.typography.titleMedium)
+                    Row {
+                        AssistChip(onClick = {}, label = {
+                            Row(horizontalArrangement = Arrangement.SpaceAround) {
+                                Icon(
+                                    imageVector = Icons.Filled.StarRate,
+                                    contentDescription = "Rate product"
+                                )
+                                Text(text = "94%", style = MaterialTheme.typography.bodySmall)
+                            }
+                        })
+                        Text(
+                            text = "117 reviews",
+                            style = MaterialTheme.typography.bodySmall.copy(fontWeight = FontWeight.Light)
+                        )
+                    }
+                    Text(
+                        dummyReview,
+                        style = MaterialTheme.typography.bodySmall
+                    )
 
                 }
+
             }
         }
     }
 }
+
+const val dummyReview =
+    "The Microsoft Xbox Series X gaming console is capable of impressing with minimal boot times and mesmerizing visual effects when playing games at up to 120 frames per second"
